8748b4e291549242d6073e505adcf19f54e6b691
Jordan Bettencourt <jbettenc@usc.edu>
2021-11-20 18:28:16 -0800

    Hover rather than click the vertical tabs

--- src/components/VerticalTabs.js
+++ src/components/VerticalTabs.js
@@ -9,7 +9,7 @@ function VerticalTabs(props) {
     <>
       <div className="flex flex-col w-auto overflow-y-scroll h-full md:ml-8">
         {itemList.map((item, i) => (
-          <div key={`item-${i}`} className={`${activeItem == i ? 'text-white' : 'text-gray-70'} select-none cursor-pointer flex-nowrap text-4xl md:text-5xl lg:text-6xl font-black leading-tight font-tabs w-max`} onClick={() => handleActiveItem(i)}>
+          <div key={`item-${i}`} className={`${activeItem == i ? 'text-white' : 'text-gray-70'} select-none cursor-pointer flex-nowrap text-4xl md:text-5xl lg:text-6xl font-black leading-tight font-tabs w-max`} onMouseEnter={() => handleActiveItem(i)}>
             {item}
           </div>
         ))}
