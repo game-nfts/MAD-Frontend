e966169eda9313237becbf7b3e87bbdc76d5103f
Jordan Bettencourt <jbettenc@usc.edu>
2022-01-25 14:48:00 -0800

    Remove mirror, add medium to footer (#14)

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import DiscordIcon from '../assets/discord.svg';
-import MirrorIcon from '../assets/mirror.svg';
+// import MirrorIcon from '../assets/mirror.svg';
 import TwitterIcon from '../assets/twitter.svg';
+import MediumIcon from '../assets/medium.svg';
 import DiscordHoverIcon from '../assets/discord_hover.svg';
-import MirrorHoverIcon from '../assets/mirror_hover.svg';
+// import MirrorHoverIcon from '../assets/mirror_hover.svg';
 import TwitterHoverIcon from '../assets/twitter_hover.svg';
+import MediumHoverIcon from '../assets/medium_hover.svg';
 import HoverImageButton from './HoverImageButton';
 
 const Footer = (props) => {
@@ -20,7 +22,7 @@ const Footer = (props) => {
               <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-center">
                 <HoverImageButton className="h-10 mx-4 cursor-pointer" onDragStart={(e) => e.preventDefault()} onClick={() => {window.open('https://discord.gg/EXd9jj9f2A', '_blank');}} defaultSrc={DiscordIcon} hoverSrc={DiscordHoverIcon} />
                 <HoverImageButton className="h-10 mx-4 cursor-pointer" onDragStart={(e) => e.preventDefault()} onClick={() => {window.open('https://twitter.com/madxyz_', '_blank');}} defaultSrc={TwitterIcon} hoverSrc={TwitterHoverIcon} />
-                <HoverImageButton className="h-10 mx-4 cursor-pointer" onDragStart={(e) => e.preventDefault()} onClick={() => {window.open('https://mirror.xyz/0x91a567BbFF43d384bAf168893FBba570b0c96b0c', '_blank');}} defaultSrc={MirrorIcon} hoverSrc={MirrorHoverIcon} />
+                <HoverImageButton className="h-10 mx-4 cursor-pointer" onDragStart={(e) => e.preventDefault()} onClick={() => {window.open('https://medium.com/metaverse-advertisement-dao', '_blank');}} defaultSrc={MediumIcon} hoverSrc={MediumHoverIcon} />
               </div>
             </div>
           </div>
