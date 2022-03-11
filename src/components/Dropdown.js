4e201268e2ff8feaa70f77dee6b391a586f6fa62
Song <fancodeexpert@outlook.com>
2022-03-11 16:27:37 +0800

    update drop down issue

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -7,17 +7,18 @@ export const DropdownWrapper = ({ header, children }) => {
   return (
     <div className="relative inline-block text-left">
       <div onClick={() => setExpanded(!isExpanded)}>{header}</div>
-      <div
-        className="absolute left-8 mt-2 w-64 rounded-2xl border border-gray-35 bg-black-20 px-4 py-3 z-30"
-        role="dropdown"
-        aria-orientation="vertical"
-        aria-labelledby="dropdown-button"
-        tabIndex={-1}
-        style={{ display: isExpanded ? "block" : "none" }}
-      >
-        {children}
+      <div className={`${isExpanded ? "block" : "hidden"}`}>
         <div
-          className="fixed top-0 left-0 z-20 w-screen h-screen"
+          className="absolute left-8 mt-2 w-64 rounded-2xl border border-gray-35 bg-black-20 px-4 py-3 z-30"
+          role="dropdown"
+          aria-orientation="vertical"
+          aria-labelledby="dropdown-button"
+          tabIndex={-1}
+        >
+          {children}
+        </div>
+        <div
+          className="fixed top-0 left-0 z-1 w-screen h-screen"
           onClick={() => setExpanded(!isExpanded)}
         />
       </div>
--- src/components/DropdownItem.js
+++ src/components/DropdownItem.js
@@ -9,16 +9,14 @@ const DropdownItem = ({ icon, name, href }) => {
 
   return (
     <div
-      className="group flex items-center px-3 py-3 text-sm"
+      className="group flex items-center px-3 py-3 text-sm rounded-lg cursor-pointer hover:bg-pink-10 hover:text-gray-500"
       role="dropdownitem"
       tabIndex={-1}
       id="dropdown-item-0"
       onClick={handleClick}
     >
       {icon}
-      <span className="font-commuter text-gray-100 font-normal text-xl">
-        {name}
-      </span>
+      <span className="font-commuter font-normal text-xl">{name}</span>
     </div>
   );
 };
