dc859535c0d22f08d61f274e0198c32c81f1ce96
Song <fancodeexpert@outlook.com>
2022-03-11 14:06:33 +0800

    fix: margin

--- src/components/RMPrice.js
+++ src/components/RMPrice.js
@@ -7,13 +7,13 @@ function RMPrice() {
     <div className="bg-black-20 w-full lg:px-5 lg:py-9 px-3 py-5 border border-gray-35 rounded-2xl font-commuter text-left">
       <p className="text-gray-85 lg:text-3xl text-base mb-5">Market Value</p>
       <div className="flex text-gray-110 items-center flex-wrap lg:mb-11 mb-3">
-        <img src={EthIcon} alt="eth-icon" className="mr-3" />
+        <img src={EthIcon} alt="eth-icon" className="mr-3 md:w-7 w-4" />
         <span className="lg:text-4xl text-lg font-semibold mr-2">1.39</span>
         <p className="lg:text-2xl text-xl font-light">(3646usd)</p>
       </div>
       <p className="text-gray-85 lg:text-3xl text-base mb-5">MAD Price:</p>
       <div className="flex text-gray-110 items-center lg:mb-11 mb-3 flex-wrap">
-        <img src={EthIcon} alt="eth-icon" className="mr-3" />
+        <img src={EthIcon} alt="eth-icon" className="mr-3 md:w-7 w-4" />
         <span className="lg:text-4xl text-lg font-semibold mr-2">0.04/Day</span>
         <p className="lg:text-2xl text-xl font-light">(105usd)</p>
       </div>
@@ -23,10 +23,10 @@ function RMPrice() {
           <img
             src={EthIcon}
             alt="eth-icon"
-            className="mr-4 md:w-4 sm:w-7 sm:h-11"
+            className="mr-4 md:w-7 w-4 sm:h-11"
           />
           <input
-            className="lg:w-40 w-24 lg:h-16 h-8 bg-transparent border border-white border-opacity-30 rounded-md text-2xl px-1"
+            className="lg:w-40 w-24 lg:h-16 h-8 bg-transparent border border-white border-opacity-30 rounded-md lg:text-2xl text-xl px-1"
             type="text"
           />
           <span className="ml-2.5 lg:text-xl text-sm mt-auto">- -Days</span>
